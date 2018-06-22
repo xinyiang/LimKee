4644f87c0c1951cc2905739dede16f137d70c200
Ang Xin Yi <xinyi.ang.2015@smu.edu.sg>
2018-06-22 23:34:18 +0800

    Design User Profile UI and pass language bundle

--- app/src/main/java/com/limkee/login/LoginActivity.java
+++ app/src/main/java/com/limkee/login/LoginActivity.java
@@ -1,38 +1,52 @@
 package com.limkee.login;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.net.Uri;
 import android.widget.Switch;
 import android.widget.TextView;
-
 import com.limkee.BaseActivity;
 import com.limkee.R;
 import com.limkee.catalogue.CatalogueFragment;
 import com.limkee.locale.MyContextWrapper;
-
 import java.security.MessageDigest;
 
 public class LoginActivity extends BaseActivity implements
         CatalogueFragment.OnFragmentInteractionListener {
     EditText companycode, password;
     Switch switchCtrl;
+    private CheckBox saveLoginCheckBox;
+    private SharedPreferences loginPreferences;
+    private SharedPreferences.Editor loginPrefsEditor;
+    private Boolean saveLogin;
+    static String isEnglish = "No";
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final Context context = getApplicationContext();
         setContentView(R.layout.activity_login);
-        final SharedPreferences settings = getSharedPreferences("switchkey", 0);
-
         companycode = findViewById(R.id.companyCode);
         password = findViewById(R.id.etPassword);
+        final SharedPreferences settings = getSharedPreferences("switchkey", 0);
+
+        saveLoginCheckBox = findViewById(R.id.saveLoginCheckBox);
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        loginPrefsEditor = loginPreferences.edit();
+        saveLogin = loginPreferences.getBoolean("saveLogin", false);
+
+        if (saveLogin == true) {
+            companycode.setText(loginPreferences.getString("username", ""));
+            password.setText(loginPreferences.getString("password", ""));
+            saveLoginCheckBox.setChecked(true);
+        }
 
         switchCtrl = (Switch) findViewById(R.id.language);
         switchCtrl.setChecked(settings.getBoolean("switchkey", false));
@@ -43,6 +57,11 @@ public class LoginActivity extends BaseActivity implements
                 SharedPreferences.Editor editor = settings.edit();
                 editor.putBoolean("switchkey", isChecked);
                 editor.apply();
+                if(isChecked){
+                    isEnglish = "Yes";
+                } else {
+                    isEnglish = "No";
+                }
                 reStart(context);
                 overridePendingTransition(0, 0);
             }
@@ -50,20 +69,38 @@ public class LoginActivity extends BaseActivity implements
     }
 
     public void login(View view){
-
         String code = companycode.getText().toString();
         String pwd = password.getText().toString();
 
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(companycode.getWindowToken(), 0);
+
+        if (saveLoginCheckBox.isChecked()) {
+            loginPrefsEditor.putBoolean("saveLogin", true);
+            System.out.println(loginPreferences.getBoolean("saveLogin", false)+"wow");
+            loginPrefsEditor.putString("username", code);
+            loginPrefsEditor.putString("password", pwd);
+            loginPrefsEditor.commit();
+        } else {
+            loginPrefsEditor.clear();
+            loginPrefsEditor.commit();
+        }
+
         //validate fields are not blank
-         if (code.equals("") || pwd.equals("")){
+        if (code.equals("") || pwd.equals("")){
             TextView pwdValidate = findViewById(R.id.pwdvalidation);
-            pwdValidate.setText("Please fill in Company Code and/or Password");
+            if (isEnglish.equals("Yes")){
+                pwdValidate.setText("Please fill in Company Code and/or Password");
+            } else {
+                pwdValidate.setText(" 请输入公司代码和/或密码");
+            }
+
         } else {
             //validate credentials to login
             pwd = getSha256(pwd);
             String type = "login";
             BackgroundLogin bl = new BackgroundLogin(this);
-            bl.execute(type, code, pwd);
+            bl.execute(type, code, pwd, isEnglish);
         }
     }
 
@@ -92,6 +129,8 @@ public class LoginActivity extends BaseActivity implements
     public static void reStart(Context context) {
         Intent intent = new Intent(context, LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("language", isEnglish);
         context.startActivity(intent);
+
     }
 }
