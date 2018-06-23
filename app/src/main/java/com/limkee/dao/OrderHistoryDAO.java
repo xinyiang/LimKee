f3bb3f47191c1233e9a34b5b85489c278705b6df
Yang Miaozi <miaozi.yang.2015@sis.smu.edu.sg>
2018-06-23 15:43:31 +0800

    change order history

--- app/src/main/java/com/limkee/dao/OrderHistoryDAO.java
+++ app/src/main/java/com/limkee/dao/OrderHistoryDAO.java
@@ -15,7 +15,7 @@ public class OrderHistoryDAO {
 
     public static ArrayList<Order> historyOrdersList = new ArrayList<Order>();
     public static OrderDetails od;
-    public static OrderQuantity oq;
+    public static ArrayList<OrderQuantity> oq;
 
     public static Order create(String OrderID, String deliveryDate, int noOfItems) {
 
@@ -30,7 +30,7 @@ public class OrderHistoryDAO {
         ArrayList<OrderQuantity> oQList = orderQtyDAO.getOrderQuantity();
         for (OrderQuantity o : oQList){
             if (o.getOrderID().equals(OrderID)){
-                oq = o;
+                oq.add(o);
             }
         }
 
