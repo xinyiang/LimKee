f129ba3e5bc4c4fcd949e2729903b384a85031bd
Yang Miaozi <miaozi.yang.2015@sis.smu.edu.sg>
2018-07-04 00:00:57 +0800

    update payment

--- app/src/main/java/com/limkee/payment/ConfirmationActivity.java
+++ app/src/main/java/com/limkee/payment/ConfirmationActivity.java
@@ -1,23 +1,58 @@
 package com.limkee.payment;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
-import com.limkee.R;
 
-public class ConfirmationActivity extends AppCompatActivity {
+import com.limkee.BaseActivity;
+import com.limkee.R;
+import com.limkee.navigation.NavigationActivity;
+import com.limkee.order.ConfirmOrderActivity;
+
+public class ConfirmationActivity extends BaseActivity {
     public static Bundle myBundle = new Bundle();
     String result;
+    Context context;
+    double tp = 0.00;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
         myBundle = getIntent().getExtras();
+        context = getApplicationContext();
         result = myBundle.getString("result");
+        tp = getIntent().getDoubleExtra("totalPayable",0.00);
         TextView pmtResult = ((Activity)this).findViewById(R.id.pmtResult);
-        pmtResult.setText(result);
+        TextView description = ((Activity)this).findViewById(R.id.description);
+        ImageView iv = findViewById(R.id.status);
+        if (result.equals("success")){
+            pmtResult.setText(getResources().getString(R.string.payment_successful));
+            description.setText(String.format("$%.2f", tp) + getResources().getString(R.string.payment_successful_description));
+            iv.setImageResource(R.drawable.success);
+        }else{
+            //pmtResult.setText(getResources().getString(R.string.payment_unsuccessful));
+            //description.setText(String.format("$%.2f", tp) + getResources().getString(R.string.payment_unsuccessful_description));
+            iv.setImageResource(R.drawable.fail);
+        }
+
+    }
+
+    public void backTo(View view){
+        if (result.equals("success")){
+            Intent it = new Intent(getApplicationContext(), NavigationActivity.class);
+            context.startActivity(it);
+            this.finish();
+        }else{
+            Intent it = new Intent(getApplicationContext(), PaymentActivity.class);
+            it.putExtra("totalPayable", tp);
+            context.startActivity(it);
+            this.finish();
+        }
     }
 
 }
--- app/src/main/java/com/limkee/payment/PaymentFragment.java
+++ app/src/main/java/com/limkee/payment/PaymentFragment.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -62,6 +66,7 @@ public class PaymentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_payment, container, false);
+
         return view;
     }
 
